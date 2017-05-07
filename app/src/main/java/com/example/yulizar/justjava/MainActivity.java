ef97b705ec9284f57aaab3ba08da5673b05e8dfd
yulizar <erwinyulizar14@gmail.com>
2017-05-07 11:33:21 +0700

    First Commit

---
+++ app/src/main/java/com/example/yulizar/justjava/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.yulizar.justjava;
+
+import android.icu.text.NumberFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int quantity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void Increment(View view){
+        quantity = getQuantity(view);
+        quantity++;
+        display(quantity);
+//        displayPrice(quantity);
+    }
+
+    public void Decrement(View view){
+        quantity = getQuantity(view);
+        quantity--;
+        display(quantity);
+//        displayPrice(quantity);
+    }
+
+    public void submitOrder(View view){
+        displayPrice(quantity*5);
+    }
+
+    /*
+
+    *This Method return number of quantity
+     */
+
+    public int getQuantity(View view){
+        TextView valueTextView = (TextView) findViewById(R.id.value);
+
+        return (Integer) Integer.parseInt(valueTextView.getText().toString());
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+
+    private void display(int number) {
+        TextView valueTextView = (TextView) findViewById(R.id.value);
+
+//        int number = (Integer) Integer.parseInt(valueTextView.getText().toString());
+//        number++;
+        valueTextView.setText("" + number);
+    }
+
+    private void displayPrice(int quantity){
+        TextView priceValueTextView = (TextView) findViewById(R.id.price_value);
+        priceValueTextView.setText(NumberFormat.getCurrencyInstance().format(quantity));
+    }
+}
